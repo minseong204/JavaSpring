a325b944e738261a3235e7bd5e0844624739dbda
minseong204 <minseong2083@icloud.com>
2023-06-27 13:43:33 +0900

    Session3. 스프링 핵심원리 이해2 - (마무리 - 스프링으로 전환)

--- java/hello/core/AppConfig.java
+++ java/hello/core/AppConfig.java
@@ -8,22 +8,28 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());     // 생성자 주입
     }
 
-    private static MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
@@ -31,3 +37,8 @@ public class AppConfig {
 
 
 }
+
+/*
+* @Configuration → 설정정보를 담당함
+* 각각의 메서드에 @Bean을 달아주게 되면 각각의 메서드들이 컨테이너에 등록이 됌
+*/
